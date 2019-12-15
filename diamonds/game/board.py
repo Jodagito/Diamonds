19e4082d2423bc9a4ee1adc9760a14e7e2242419
Daniel <jdoagito@gmail.com>
2019-12-15 13:06:44 -0500

    board module

---
+++ diamonds/game/board.py
@@ -0,0 +1,21 @@
+from random import randint
+
+board = []
+
+
+def board_creation():
+    while len(board) < 8:
+        row = []
+        while len(row) < 8:
+            cell = randint(1, 5)
+            row.append(cell)
+        board.append(row)
+    return board
+
+
+def show_board():
+    for row in range(len(board)):
+        for column in range(len(board[row])):
+            print(board[row][column], end=" ")
+        print()
+    print()
